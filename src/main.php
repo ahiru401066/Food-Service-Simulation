9eaf9b3c427a35595ebbd5a119b3077578eb56d4
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-03-15 01:56:21 +0900

    不要なフォルダ・ファイルの削除

--- src/main.php
+++ src/main.php
@@ -1,10 +1,17 @@
 <?php
 
 spl_autoload_extensions(".php");
-spl_autoload_register();
+spl_autoload_register(function($class) {
+    $base_dir = __DIR__ ."/";
+    $file = $base_dir . str_replace("\\","/", $class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
 
 $pizza = new FoodItems\Pizza("tomato", "this is a tomato pizza", 5.0);
 $pasta = new FoodItems\Pasta("Spaghetti", "this is spaghetti", 10.0);
+$person = new Persons\Employees\Employee("tomoya", 22, "yamagata", 1, 10.0);
 
 echo $pizza->getCategory().PHP_EOL;
-echo $pasta->getCategory().PHP_EOL;
